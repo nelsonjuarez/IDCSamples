b053a74078624c8b7b30adf9d5eb90f6ae68beb9
nelsonjuarez <143960493+nelsonjuarez@users.noreply.github.com>
2024-07-10 00:03:18 +0200

    Update rawEtlJobGlue.py

--- rawEtlJobGlue.py
+++ rawEtlJobGlue.py
@@ -1,5 +1,4 @@
 import sys
-import requests
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -9,14 +8,13 @@ from awsglue.dynamicframe import DynamicFrame
 import logging
 import boto3
 import re
-import json
 
 # Initialize logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def get_parameters():
-    return getResolvedOptions(sys.argv, ['JOB_NAME', 'TABLE_NAME', 'API_URL', 'API_KEY', 'RAW_PATH', 'DATABASE_NAME'])
+    return getResolvedOptions(sys.argv, ['JOB_NAME', 'TABLE_NAME', 'LANDING_PATH', 'RAW_PATH', 'DATABASE_NAME'])
 
 def initialize_glue_job(args):
     sc = SparkContext()
@@ -26,34 +24,13 @@ def initialize_glue_job(args):
     job.init(args['JOB_NAME'], args)
     return sc, glueContext, spark, job
 
-def get_data_from_api(api_url, api_key):
-    headers = {'Authorization': f'Bearer {api_key}'}
+def read_data(spark, source_path):
     try:
-        response = requests.get(api_url, headers=headers)
-        response.raise_for_status()  # Raise an HTTPError on bad response
-        logger.info("Data fetched successfully from API.")
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Error fetching data from API: {str(e)}")
-        raise
-
-def read_data_from_api(spark, api_url, api_key):
-    data = get_data_from_api(api_url, api_key)
-    
-    # Log the fetched data for debugging
-    logger.info(f"Fetched data: {json.dumps(data, indent=2)}")
-
-    try:
-        # Flatten the JSON structure to a list of records for Spark DataFrame
-        records = [{"currency": k, "rate": float(v), "timestamp": data["timestamp"], "base": data["base"]}
-                   for k, v in data["rates"].items()]
-
-        # Convert the list of records to a Spark DataFrame
-        df = spark.createDataFrame(records)
-        logger.info("Data read successfully from API.")
+        df = spark.read.csv(source_path, header=True, inferSchema=True)
+        logger.info("Data read successfully.")
         return df
     except Exception as e:
-        logger.error(f"Error processing data from API: {str(e)}")
+        logger.error(f"Error reading data from {source_path}: {str(e)}")
         raise
 
 def sanitize_data(df):
@@ -97,8 +74,7 @@ def main():
     args = get_parameters()
     
     table_name = args['TABLE_NAME']
-    api_url = args['API_URL']
-    api_key = args['API_KEY']
+    landing_path = args['LANDING_PATH']
     raw_path = args['RAW_PATH']
     database_name = args['DATABASE_NAME']
     
@@ -108,23 +84,14 @@ def main():
     
     create_database_if_not_exists(database_name)
     
-    logger.info(f"Fetching data from API: {api_url}")
+    source_path = landing_path
+    logger.info(f"Reading data from {source_path}")
     
-    df = read_data_from_api(spark, api_url, api_key)
+    df = read_data(spark, source_path)
     
     logger.info("Sanitizing data...")
     sanitized_df = sanitize_data(df)
     logger.info("Data sanitized successfully.")
-    
-    # Truncating destination bucket)
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(raw_path.split('/')[2])
-    prefix = '/'.join(raw_path.split('/')[3:])
-    
-    for obj in bucket.objects.filter(Prefix=prefix):
-        s3.Object(bucket.name, obj.key).delete()
-    
-    sanitized_df.write.mode("overwrite").parquet(raw_path)
     
     logger.info("Saving data to Glue Data Catalog...")
     save_to_catalog(glueContext, sanitized_df, database_name, table_name, raw_path)
